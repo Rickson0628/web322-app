edbc060ecd6ef3cb31019fe8040054e581fb7366
Rickson Bozar <rbozar@myseneca.ca>
2024-10-14 20:21:35 -0400

    TRANSFERRED TO GIT REPO

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or
+distributed to other students.
+Name: Rickson Bozar
+Student ID: 167549237
+Date: 2024-10-14
+Vercel Web App URL:
+GitHub Repository URL: https://github.com/Rickson0628/web322-app.git
+********************************************************************************/
+
 const express = require('express');
 const path = require('path');
 const storeService = require('./store-service');
